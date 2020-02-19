b5c041535bd6d2c1ee22dce2f072866d3b749f2a
Joey <joeyskuo@gmail.com>
2020-02-18 21:46:45 -0800

    Search view implemented in main controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base' ;
+
+// Global State
+const state = {};
+
+const controlSearch = async () => {
+
+    const query = searchView.getInput();
+
+    if(query) {
+        state.search = new Search(query);
+        await state.search.getResults();
+        console.log(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
